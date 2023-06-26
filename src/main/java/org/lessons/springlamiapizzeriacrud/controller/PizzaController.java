85b6e204745a6150550b583d0d4e8ae633fc08a1
marcantonio99 <marcantonio8799@gmail.com>
2023-06-26 15:43:45 +0200

    by entering the id of a pizza in the search bar I get its details

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
@@ -3,12 +3,16 @@ package org.lessons.springlamiapizzeriacrud.controller;
 import org.lessons.springlamiapizzeriacrud.Pizza;
 import org.lessons.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizze")
@@ -22,5 +26,18 @@ public class PizzaController {
         List<Pizza> pizze = pizzaRepository.findAll();
         model.addAttribute("pizzaList", pizze);
         return "/pizze/list";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable("id") Integer pizzaId, Model model){
+
+         Optional<Pizza> result = pizzaRepository.findById(pizzaId);
+         if (result.isPresent()){
+             model.addAttribute("pizza", result.get());
+
+             return "/pizze/detail";
+         }else {
+             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza id" + pizzaId + "not found");
+         }
     }
 }
